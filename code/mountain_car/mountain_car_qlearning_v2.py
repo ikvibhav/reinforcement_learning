a27d12fe7c4f2ae84bfa7f13604ff30299663f9e
vibhav <vibhav.innakedege@alfen.com>
2024-05-13 15:29:22 +0200

    13052024 commit 2

--- code/mountain_car/mountain_car_qlearning_v2.py
+++ code/mountain_car/mountain_car_qlearning_v2.py
@@ -38,7 +38,16 @@ Q_TABLE = np.random.uniform(
 )
 LEARNING_RATE = 0.1
 DISCOUNT = 0.95
-EPISODES = 25000
+EPISODES = 25
+EPSILON = 1
+START_EPSILON_DECAYING = 1
+END_EPSILON_DECAYING = EPISODES // 2
+epsilon_decay_value = EPSILON / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
+
+# For stats
+EPISODE_STATS = 5
+episode_rewards_list = []
+summarised_dictionary = {"ep": [], "avg": [], "min": [], "max": []}
 
 
 def get_discrete_state(state):
@@ -46,13 +55,20 @@ def get_discrete_state(state):
     return tuple(discrete_state.astype(int))
 
 
-for i in tqdm(range(EPISODES)):
+for episode in tqdm(range(EPISODES)):
+    episode_reward = 0
     terminated, truncated = False, False
     curr_state, _ = env.reset()
     curr_discrete_state = get_discrete_state(curr_state)
 
+    steps = 0
     while not terminated:
-        action = np.argmax(Q_TABLE[curr_discrete_state])
+        steps += 1
+        if np.random.random() > EPSILON:
+            action = np.argmax(Q_TABLE[curr_discrete_state])
+        else:
+            action = np.random.randint(0, env.action_space.n)
+
         new_state, reward, terminated, truncated, _ = env.step(action)
         new_discrete_state = get_discrete_state(new_state)
 
@@ -63,7 +79,38 @@ for i in tqdm(range(EPISODES)):
                 reward + DISCOUNT * max_future_q
             )
             Q_TABLE[curr_discrete_state + (action,)] = new_q
-
         elif new_state[0] >= env.goal_position:
-            logger.info(f"Goal reached at episode {i}")
-            terminated = True
+            Q_TABLE[curr_discrete_state + (action,)] = 0
+
+        curr_discrete_state = new_discrete_state
+        episode_reward += reward
+
+    episode_rewards_list.append(episode_reward)
+
+    if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
+        EPSILON -= epsilon_decay_value
+
+    if episode % EPISODE_STATS == 0:
+        avg_reward = sum(episode_rewards_list[-EPISODE_STATS:]) / len(
+            episode_rewards_list[-EPISODE_STATS:]
+        )
+        summarised_dictionary["ep"].append(episode)
+        summarised_dictionary["avg"].append(avg_reward)
+        summarised_dictionary["min"].append(min(episode_rewards_list[-EPISODE_STATS:]))
+        summarised_dictionary["max"].append(max(episode_rewards_list[-EPISODE_STATS:]))
+        logger.info(
+            f"Episode:{episode}, avg:{avg_reward}, "
+            f"min:{min(episode_rewards_list[-EPISODE_STATS:])} "
+            f"max:{max(episode_rewards_list[-EPISODE_STATS:])}, "
+            f"steps = {steps}, truncated = {truncated}, terminated = {terminated}"
+        )
+
+env.close()
+plt.plot(summarised_dictionary["ep"], summarised_dictionary["avg"], label="avg")
+plt.plot(summarised_dictionary["ep"], summarised_dictionary["min"], label="min")
+plt.plot(summarised_dictionary["ep"], summarised_dictionary["max"], label="max")
+plt.legend(loc=4)  # bottom right
+plt.title("Mountain Car Q-Learning")
+plt.ylabel("Average reward/Episode")
+plt.xlabel("Episodes")
+plt.savefig("mountain_car_qlearning_v2.png")
